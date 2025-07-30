0928a9ffd61d39b21fceaca91d680a2c3cb5ba5b
YaraZalat <yarazalat6@gmail.com>
2025-07-30 10:42:56 +0300

    Initial project structure and core setup

---
+++ lib/core/constant/assets_constant.dart
@@ -0,0 +1,15 @@
+abstract class AssetsConstant{
+  static String logo="assets/icons/logo-icon.png";
+  static String logout="assets/icons/logout-icon.png";
+  static String empty="assets/images/empty-screen.png";
+  static String timeIcon="assets/icons/timer-icon.png";
+  static String sendIcon="assets/icons/send-icon.png";
+  static String flagIcon="assets/icons/flag-icon.png";
+  static String task="assets/icons/Tasky.png";
+  static String y="assets/icons/y.png";
+  static String onboard1="assets/images/onboard1.png";
+  static String onboard2="assets/images/onboard2.png";
+  static String onboard3="assets/images/onboard3.png";
+  static String close="assets/icons/Close.png";
+
+}
